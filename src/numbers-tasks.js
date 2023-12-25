dfa4636743e23413add33f6750b57b5f643b94a1
myuko <myuko@student.21-school.ru>
2023-12-26 01:54:45 +0300

    feat: getIntegerOnString done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -515,8 +515,9 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  const parsedInt = Number.parseInt(str, base);
+  return Number.isNaN(parsedInt) ? NaN : parsedInt;
 }
 
 /**
